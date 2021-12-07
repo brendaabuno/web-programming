4167f0227962d3fafd679736e641e5f8a262be38
brendaabuno <brendaabuno025@gmail.com>
2021-12-07 14:04:53 +0800

    add

---
+++ tugas-tambahan-wp/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+
+      <Navbar />
+      <Card />
+
+    </div>
+  );
+}
+
+export default App;
